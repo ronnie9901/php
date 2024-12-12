f8e37b853d030e562a74a5e4c3280b93455b2955
Rohan nayak <148763509+ronnie9901@users.noreply.github.com>
2024-12-12 12:20:22 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,30 +1,61 @@
 <?php 
 
 
-class Config{
 
+    class Config{
+     
+        private $host = "localhost";
+        private $usermane = "root";
+        private $password = "";
+        private $datbase = "demo";
+        private $connection;
 
-    private $host ="localhost";
-    private $username= "root";
-    private $passward ="";
-    private $database ="emp";
-    private $conntection;
-
-    public function connect()
-    {
-        $this->conntection = mysqli_connect($this->host,$this->username,$this->passward,$this->database);
-
+        public function connect()
+        {
+          $this->connection = mysqli_connect($this->host,$this->usermane,$this->password,$this->datbase);
+            
        
-    }
-   
-    public function insert($name,$phone_number, $age ,$standard)
+        }
+
+        public function __construct()
     {
-        $query = "INSERT INTO employe  (name ,phone_number,age,standard)  VALUES ('$name', $phone_number ,$age ,'$standard')" ;
-         $res  = mysqli_query($this->conntection,$query);
+      $this->connect();
     }
-    public  function __construct()
+
+    public function insert ($name,$age,$course,$phone)
     {
-         $this -> connect();
+      $query = "INSERT INTO students (name,age,course,phone) VALUES('$name',$age,'$course',$phone)";
+      $res = mysqli_query($this->connection,$query);
+        if($res)
+          {
+            echo "Data Inserted Succesfully...!";
+          }
+          else
+          {
+            echo "Data Insertion Failed...!";
+          }
+    }   
+
+
+    public function fetch()
+    {
+      $query = "SELECT * FROM students";
+      $res = mysqli_query($this->connection, $query);
+      return $res;
+    }
+
+    public function delete($id)
+    {
+        $query = "DELETE FROM students WHERE id = $id";
+        $res = mysqli_query($this->connection, $query);
+        return $res;
+    }
+
+    public function update($id, $name, $age, $course, $phone)
+    {
+        $query = "UPDATE students SET name='$name', age=$age, course='$course', phone=$phone WHERE id=$id";
+        $res = mysqli_query($this->connection, $query);
+        return $res;
     }
 
 }
